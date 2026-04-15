177b1904bc06abb0890a0a182a72a075f2224cc1
Joao Victor <joaovictorgi13@gmail.com>
2026-04-15 13:37:58 -0300

    Etapas

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+
+        // declarar variaveis
+
+        // "pegar" escolha do usuario
+
+        // fazer uma escolha aleatoria para o pc
+
+        //verificar condições de vitoria
+
+        // Perguntar se quer jogar novamente
+    }
+}
