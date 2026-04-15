8cea4480a6a6b9ebf0dfca8ec4ae15050e724bfb
Joao Victor <joaovictorgi13@gmail.com>
2026-04-15 14:38:24 -0300

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
 
-        // declarar variaveis
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        // "pegar" escolha do usuario
+        String[] escolhas = {"pedra", "papel", "tesoura"};
+        String escolhaUsuario;
+        String escolhaPC;
+        String jogarNovamente = "sim";
 
-        // fazer uma escolha aleatoria para o pc
+        do {
+            System.out.print("Faça sua escolha (pedra, papel ou tesoura): ");
+            escolhaUsuario = scanner.nextLine().toLowerCase();
 
-        //verificar condições de vitoria
+            if(!escolhaUsuario.equals("pedra") && !escolhaUsuario.equals("papel") && !escolhaUsuario.equals("tesoura")){
+                System.out.println("Escolha invalida!!");
+                continue;
+            }
 
-        // Perguntar se quer jogar novamente
+            escolhaPC = escolhas[random.nextInt(3)];
+            System.out.println("Computador: " + escolhaPC);
+
+            if(escolhaUsuario.equals(escolhaPC)){
+                System.out.println("Empate!!");
+            } else if(escolhaUsuario.equals("pedra") && escolhaPC.equals("tesoura") ||
+                    escolhaUsuario.equals("papel") && escolhaPC.equals("pedra") ||
+                    escolhaUsuario.equals("tesoura") && escolhaPC.equals("papel")){
+                System.out.println("Você Ganhou!!");
+            }else {
+                System.out.println("Você Perdeu!!");
+            }
+
+            System.out.println("Jogar novamente? (sim/nao)");
+            jogarNovamente = scanner.nextLine().toLowerCase();
+
+        } while(jogarNovamente.equals("sim"));
+
+        System.out.println("Programa Encerrado!!");
+
+        scanner.close();
     }
 }
